9b151540afeb32c17cef7aa92aa5cbe0a45e0b09
amejid <abdelmejid89@gmail.com>
2023-01-01 10:30:04 +0300

    Setup linting

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
