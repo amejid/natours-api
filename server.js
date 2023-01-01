8c84e470b02567e4158d277a2f20217f5f354267
amejid <abdelmejid89@gmail.com>
2023-01-01 11:38:58 +0300

    Setup mongoose connection

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(`Server started on port ${port}`));
+const DB = process.env.DB_URL.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose.set('strictQuery', false);
+mongoose.connect(DB).then(() => {
+  console.log('DB connection successful!');
+  const port = process.env.PORT || 4000;
+  app.listen(port, () => console.log(`Server started on port ${port}`));
+});
