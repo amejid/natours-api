8035da4652c4be9561bc4ca95efe8c65d73824a9
amejid <abdelmejid89@gmail.com>
2023-01-09 13:24:04 +0300

    Add nested get route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -10,9 +10,11 @@ const {
   getTourStats,
   getMonthlyPlan,
 } = require('../controllers/tourController');
-const { createReview } = require('../controllers/reviewController');
+const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router();
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/tour-stats').get(getTourStats);
@@ -23,9 +25,5 @@ router
   .get(getTour)
   .patch(updateTour)
   .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
-
-router
-  .route('/:tourId/reviews')
-  .post(protect, restrictTo('user'), createReview);
 
 module.exports = router;
